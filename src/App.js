049507cb35298fff5d3834356abe071743190c6b
Andrew Watson <watson.arw@gmail.com>
2019-05-04 21:19:46 +1000

    Add cors headers to response

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const { getMessage, createMessage, listMessages } = require('./messages');
 
 const app = express();
 
 app.use(bodyParser.json({ strict: false }));
+app.use(cors());
 
 app.get('/messages/:id', getMessage);
 
